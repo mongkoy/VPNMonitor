d710b22f53cb71268dc348030c08665ec1a14899
Reihmon mongkoy Estremos <koydaut@gmail.com>
2016-07-20 06:07:16 +0200

    check tun0 if exists before starting any application

--- vpnmonitory.py
+++ vpnmonitory.py
@@ -7,14 +7,19 @@ from subprocess import Popen, PIPE
 import sys
 import os
 import signal
+import getopt
+import time
+import pdb
 
 application=sys.argv[1]
+__procopenvpn=0
+__procapp=0
 	
 def vpn_handler(*args, **kwargs):
     print "Caught signal (in catchall handler) "
     print args
     cmd = list()
-    cmd = ["pgrep",application]
+    cmd = ["pgrep","-f",application]
 
     if args[0] == 7 and args[1] == 2:
         p = Popen(cmd, stdout=PIPE)
@@ -24,19 +29,69 @@ def vpn_handler(*args, **kwargs):
         return
     if args[0] == 5 and args[1] == 1:
         print 'starting tribler'
-        p = Popen([application], stdout=PIPE)
+        p = Popen(["nohup",application,"&"])
         out, err = p.communicate()
         print out
+
+def startOpenVpn(optarg,app):
+    cmd = list()
+    cmd = ["nohup","/usr/sbin/openvpn","--daemon", "--config",optarg,"--log","/var/log/openvpn.log"]
+    global __procopenvpn
+    print optarg
+    __procopenvpn = Popen(cmd, close_fds=True,preexec_fn=os.setsid)
+    time.sleep(5)
+    print app
+    cmd = ["nohup",app,"&"]
+    global __procapp
+    __procapp = Popen(cmd, close_fds=True,preexec_fn=os.setsid)
+    time.sleep(2)
+
+def mainloop():
+    time.sleep(5)
+    while(True):
+        cmd = ["ifconfig","tun0"]
+        proc = Popen(cmd, stdin=subprocess.PIPE,stdout=subprocess.PIPE)
+        out, err = proc.communicate()
+        if("UP POINTOPOINT" in out):
+            time.sleep(2)
+            continue
+        else:
+            print "killing " + str(__procapp.pid)
+            os.killpg(__procapp.pid, signal.SIGUSR1)
+            time.sleep(2)
+            print "killing " + str(__procopenvpn.pid)
+            os.killpg(__procopenvpn.pid, signal.SIGUSR1)
+            os.killpg(__procopenvpn.pid+1, signal.SIGUSR1)
+            break
+
+
+
 
 if __name__ == "__main__":
     
     #application to start or kill when vpn is on or off 
-        
-    bus= dbus.SystemBus()
-    object = bus.get_object('org.freedesktop.NetworkManager',
-            '/org/freedesktop/NetworkManager/VPN')
-    bus.add_signal_receiver(vpn_handler, dbus_interface = 'org.freedesktop.NetworkManager.VPN.Connection', message_keyword='VpnStateChanged')
-    loop = GObject.MainLoop()
-    loop.run()
+    opts, args = getopt.getopt(sys.argv[1:], "oan:h", ["openvpn=","application=","networkManager="])
+    app = ""
+    for opt, arg in opts:
+        if opt in ("-o", "--openvpn"):             
+            pdb.set_trace()
 
+        #start openvpn via commandline
+            startcmd = True
+            optarg = arg
+        elif opt in ("-a","--application"):
+            app = arg
+        elif opt in ("-n","--networkManager"):
+            bus= dbus.SystemBus()
+            object = bus.get_object('org.freedesktop.NetworkManager',
+            '/org/freedesktop/NetworkManager/VPN')
+            bus.add_signal_receiver(vpn_handler, dbus_interface = 'org.freedesktop.NetworkManager.VPN.Connection', message_keyword='VpnStateChanged')
+            loop = GObject.MainLoop()
+            loop.run()
+            sys.exit()
+        else:
+            assert False, "unhandled option"
+    if(startcmd):
+        startOpenVpn(optarg,app)
+        mainloop()
 
